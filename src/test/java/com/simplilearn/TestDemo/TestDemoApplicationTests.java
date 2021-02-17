b4f6e31979ac01b9cbf31aebdfd121eb134d6240
student <yabing.bi@ngc.com>
2021-02-17 06:54:26 +0000

    Update .java

--- src/test/java/com/simplilearn/TestDemo/TestDemoApplicationTests.java
+++ src/test/java/com/simplilearn/TestDemo/TestDemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.simplilearn.TestDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TestDemoApplicationTests {
